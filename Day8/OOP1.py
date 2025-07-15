f14f0801da2fcc9a088151c9b055916ce7c59c20
kb2875 <kiranbailey@gmail.com>
2025-07-15 10:01:09 +0530

    Day 8

---
+++ Day8/OOP1.py
@@ -0,0 +1,8 @@
+#class without attributes
+class Hello:
+    def printhello(self):
+        print("Hello BMS")
+
+obj1=Hello()
+obj2=Hello()
+obj1.printhello()
---
+++ Day8/OOP2.py
@@ -0,0 +1,13 @@
+#class with attributes
+#When there are attributes we need to create a constructor
+class student:
+    def __init__(self, name, USN):
+        self.name=name
+        self.USN=USN
+    def Display(self):
+        print("Name is:",self.name, "USN is:",self.USN)
+
+s1=student("Kiran", 30)
+s2=student("Ananth", 19)
+s2.Display()
+s1.Display()
